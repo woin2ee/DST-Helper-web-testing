f240387b54d9bb1f3d2860499f5ecf46535b1d21
Jaewon Yun <woin2ee@gmail.com>
2024-09-08 17:52:20 +0900

    Frune incomplete features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d409dcdc9b4129f5484832b013f11540080e5c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2205786534"
+    serviceWorkerVersion: "1295287637"
   }
 });
