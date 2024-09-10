a99675f34d8151256523664767faad5cd3df1c2d
Jaewon Yun <woin2ee@gmail.com>
2024-09-10 19:33:35 +0900

    Introduce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d409dcdc9b4129f5484832b013f11540080e5c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1295287637"
+    serviceWorkerVersion: "3259821505"
   }
 });
