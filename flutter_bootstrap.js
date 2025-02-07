95329896be0fb5294be4dd85c537674dd8197aed
Jaewon Yun <woin2ee@gmail.com>
2025-02-07 17:47:03 +0900

    Test CD with mise tasks

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"d409dcdc9b4129f5484832b013f11540080e5c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4255184766"
+    serviceWorkerVersion: "1498763103"
   }
 });
